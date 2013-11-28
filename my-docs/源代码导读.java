3614aebbf2de5a549ed37d6026b3c8082920e9f2
Honghua Zhu <zhh200910@gmail.com>
2013-11-28 21:19:24 +0800

    删除2.0.0

--- my-docs/源代码导读.java
+++
@@ -1,3 +0,0 @@
-从哪看起:
-org.apache.cassandra.service.CassandraDaemon.main(String[])
-
--- my-test/my/test/start/CassandraDaemonStop.java
+++
@@ -1,9 +0,0 @@
-package my.test.start;
-
-import org.apache.cassandra.service.CassandraDaemon;
-
-public class CassandraDaemonStop {
-    public static void main(String[] args) {
-        CassandraDaemon.stop(new String[] {}); // 无效，不能这样停止CassandraDaemon
-    }
-}
